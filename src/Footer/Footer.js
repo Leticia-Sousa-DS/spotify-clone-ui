396067a714736ab6c22e35488256c78798538370
Leticia-Sousa-DS <leticiasousassilva@hotmail.com>
2025-06-21 19:37:35 -0300

    versão com  react

---
+++ src/Footer/Footer.js
@@ -0,0 +1,20 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer class="footer">
+        <div class="text">
+            <p class="footer__title">Testar o Premium de graça</p>
+            <p class="footer__subtitle">
+                Inscreva-se para curtir música ilimitada e podcasts só com alguns anúncios. 
+                Não precisa de cartão de crédito.
+            </p>
+        </div>
+        <div class="button">
+            <button type="button">Inscreva-se grátis</button>
+        </div>
+    </footer>
+    )
+};
+
+export default Footer;
